2824d91f8c72a26977a5c1e6704a00c7434de04f
Rahmad Alfian Syahputra <rahmadalfiansyahputra48@gmail.com>
2021-09-10 15:21:48 +0700

    Update apiController.js

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -66,7 +66,7 @@ module.exports = {
   selectCategory: async (req, res) => {
     try {
       const { name } = req.params;
-      const item = await Item.find({ categoryId: categoryId.name });
+      const item = await Item.find({ categoryId: name });
       res.status(200).json({
         item,
       });
