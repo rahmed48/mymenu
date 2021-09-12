e52a673931677e378ed177b2b21f9aa039b84b5d
Rahmad Alfian Syahputra <rahmadalfiansyahputra48@gmail.com>
2021-09-12 17:03:19 +0700

    update api.js

--- routes/api.js
+++ routes/api.js
@@ -3,12 +3,12 @@ const apiController = require("../controllers/apiController");
 // const { upload } = require("../middlewares/multer");
 
 router.get("/item", apiController.item);
-router.get("/detail-item/:id", apiController.detailItem);
-router.get("/select-category/:id", apiController.selectCategory);
 router.get("/toko", apiController.toko);
 router.get("/home", apiController.home);
-router.get("/pesanan", apiController.pesanan);
 router.post("/cart", apiController.cart);
+// router.get("/pesanan", apiController.pesanan);
+// router.get("/detail-item/:id", apiController.detailItem);
+// router.get("/select-category/:id", apiController.selectCategory);
 // router.get("/detail-page/:id", apiController.detailPage);
 
 module.exports = router;
